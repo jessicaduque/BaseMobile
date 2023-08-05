bf1ecdf4580f1b3e589c215bd639d3c2e8cbeea5
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2023-08-04 22:04:34 -0300

    Geração randomizada posição bola boliche e tipo da esfera

--- Assets/Scripts/GerenciadorDeObjetos.cs
+++ Assets/Scripts/GerenciadorDeObjetos.cs
@@ -4,12 +4,62 @@ using UnityEngine;
 
 public class GerenciadorDeObjetos : MonoBehaviour
 {
-    public GameObject Bola;
+    public List<GameObject> MinhasEsferas;
+    public float meuTempo;
+    public float tempoCriacaoEsfera = 1f;
 
     void Update()
     {
-        GameObject Bol = Instantiate(Bola, transform.position, Quaternion.identity);
-        Destroy(Bol, 2f);
+        //CriarBola();
+        CriarBolaDificil();
     }
 
+    void CriarBola()
+    {
+        meuTempo += Time.deltaTime;
+
+        // Cria uma esfera a cada tempo determinado
+        if (meuTempo > tempoCriacaoEsfera)
+        {
+            float posX = Random.Range(-2, 2.1f);
+            Vector2 novaPos = new Vector3(posX, 3.4f);
+            int tipoEsfera = Random.Range(0, MinhasEsferas.Count);
+            GameObject Bol = Instantiate(MinhasEsferas[tipoEsfera], novaPos, Quaternion.identity);
+            Destroy(Bol, 2f);
+            meuTempo = 0;
+        }
+    }
+
+    void CriarBolaDificil()
+    {
+        meuTempo += Time.deltaTime;
+
+        // Cria uma esfera a cada tempo determinado
+        if (meuTempo > tempoCriacaoEsfera)
+        {
+            float posX = Random.Range(-2, 2.1f);
+            Vector2 novaPos = new Vector3(posX, 3.4f);
+            int esferaEscolhida = 0;
+            int tipoEsfera = Random.Range(0, 101);
+            if(tipoEsfera < 80)
+            {
+                esferaEscolhida = 0;
+            }
+            else if(tipoEsfera < 90)
+            {
+                esferaEscolhida = 1;
+            }
+            else if (tipoEsfera < 98)
+            {
+                esferaEscolhida = 2;
+            }
+            else
+            {
+                esferaEscolhida = 3;
+            }
+            GameObject Bol = Instantiate(MinhasEsferas[esferaEscolhida], novaPos, Quaternion.identity);
+            Destroy(Bol, 2f);
+            meuTempo = 0;
+        }
+    }
 }
